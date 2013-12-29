4e3efc261477993d1532902f422a228689982f9e
Mattias Petter Johansson <mpj@spotify.com>
2013-12-29 17:50:49 +0100

    syntactic sugar

--- runner.js
+++ runner.js
@@ -1,37 +1,91 @@
 var Q = require('q')
+var find = require('mout/array/find')
+var partial = require('mout/function/partial')
+var deepEquals = require('mout/object/deepEquals')
+
+function findTransformByName(module, transformName) {
+  return find(module.transforms, function(t) {
+    return t.name === transformName
+  })
+}
+
+function sendUntilDone(module, expectations, channel, message, events) {
+  var deferred = Q.defer()
+
+  var events = events || []
+
+  var timelineEvent = {}
+  events.push(timelineEvent)
+
+  timelineEvent.received = {
+    channel: channel,
+    message: message
+  }
+
+  // 1. Expectations always have first dibs on messages sent
+  // to channels.
+  var expectation = find(expectations, { channel: channel })
+  if (expectation) {
+    timelineEvent.expectation = {
+      message: expectation.message,
+      match: deepEquals(expectation.message, message)
+    }
+    deferred.resolve(events)
+  } else {
+
+    // 2. Try implicit routing if the channel name
+    // exactly matches transform name
+    var transform = findTransformByName(module, channel)
+
+    // 3. See if this channel is routed to a transform
+    // explicitly by one of the routes
+    if (!transform) {
+      var route = find(module.routes, { channel: channel })
+      if (route)
+        transform = findTransformByName(module, route.transform)
+    }
+
+    if (transform) {
+      timelineEvent.transform = transform.name
+      var work = {
+        message: message,
+        done: function(channel, message) {
+          timelineEvent.sent = {
+            channel: channel,
+            message: message
+          }
+          sendUntilDone(module, expectations, channel, message, events).then(deferred.resolve)
+        }
+      }
+      transform(work)
+    } else {
+      // Could not route to a transform.
+      deferred.resolve(events)
+    }
+  }
+
+  return deferred.promise
+}
+
+function runWorld(module, world) {
+  return sendUntilDone(module, world.expectations, 'start', true).then(function(timeline) {
+    return {
+      world: { label: world.label },
+      events: timeline
+    }
+  })
+}
 
 var Runner = {
   run: function(module) {
-    var deferred = Q.defer()
-    deferred.resolve({
-      timelines: [{
-        world: { label: module.worlds[0].label },
-        events: [
-          {
-            sent: {
-              channel: 'start',
-              message: true
-            }
-          },
-          {
-            received: {
-              channel: 'start',
-              message: true
-            },
-            transform: 'add_five_and_seven',
-            sent: {
-              channel: 'add',
-              message: {
-                a: 5,
-                b: 7
-              }
-            }
-          }
-        ]
-      }]
+    var runWorldInModule = partial(runWorld, module)
+    var whenTimelines = Q.all(module.worlds.map(runWorldInModule))
+    return whenTimelines.then(function(timelines) {
+      return {
+        timelines: timelines
+      }
     })
 
-    return deferred.promise
   }
 }
 
--- spec.js
+++ spec.js
@@ -3,55 +3,105 @@ chai.Assertion.includeStack = true;
 var Q = require('q')
 chai.should()
 
+
+// TODO: Support for multiple expectations on one channel
+// TODO: Support deepEquals for arrays
+// TODO: Support combining routes
+// TODO: Implement foreigners
+
 var Runner = require('./runner.js')
-
-
-
-
 
 var runner = Object.create(Runner)
 
-runner.run({
-  transforms: [
-    function add(work) {
-      work.done({
+var World = {
+  expectations: [],
+  expectation: function(e) { this.expectations.push(e); return this }
+}
+
+var Module = {
+  routes: [],
+  transforms: [],
+  worlds: [],
+  world:     function(w) { this.worlds    .push(w); return this },
+  transform: function(t) { this.transforms.push(t); return this },
+  route:     function(r) { this.routes    .push(r); return this }
+}
+
+
+var module =
+  Object.create(Module)
+    .route({
+      channel: 'start',
+      transform: 'add_five_and_seven'
+    })
+    .transform(function add_five_and_seven(work) {
+      work.done('add', { a: 5, b: 7 })
+    })
+    .transform(function add(work) {
+      work.done('add_success', {
         result: work.message.a + work.message.b
       })
-    },
-    function add_five_and_seven(work) {
-      work.done('add', { a: 5, b: 7 })
-    }
-  ],
-  routes: [{
-    channel: 'start',
-    transform: 'add_five_and_seven'
-  }],
-  worlds: [{
-    label: 'Playground',
-    interceptors: [{
-      channel: 'add successful',
-      expect: {
-        result: 7
-      }
-    }]
-  }]
-}).then(function(result) {
+    })
+    .world({
+      label: 'Playground',
+      expectations: [{
+        channel: 'add_success',
+        message: {
+          result: 12
+        }
+      }]
+    })
+    .world({
+      label: 'Test failure',
+      expectations: [{
+        channel: 'add_success',
+        message: {
+          result: 13 // <- not 12!
+        }
+      }]
+    })
+
+
+
+
+runner.run(module).then(function(result) {
+  console.log("result is", JSON.stringify(result, null, 2))
   var timeline = result.timelines[0]
   timeline.world.label.should.equal('Playground')
   var e = timeline.events
 
-  e[0].sent.channel.should.equal('start')
-  e[0].sent.message.should.equal(true)
+  e[0].received.channel.should.equal('start')
+  e[0].received.message.should.equal(true)
+  e[0].transform.should.equal('add_five_and_seven')
+  e[0].sent.channel.should.equal('add')
+  e[0].sent.message.a.should.equal(5)
+  e[0].sent.message.b.should.equal(7)
 
-  e[1].received.channel.should.equal('start')
-  e[1].received.message.should.equal(true)
-  e[1].transform.should.equal('add_five_and_seven')
-  e[1].sent.channel.should.equal('add')
-  e[1].sent.message.a.should.equal(5)
-  e[1].sent.message.b.should.equal(7)
+  // There is no explicit route for add, but
+  // if channel name matches perfectly, the message is routed
+  // to the transform with the same name
+  e[1].received.channel.should.equal('add')
+  e[1].received.message.a.should.equal(5)
+  e[1].received.message.b.should.equal(7)
+  e[1].transform.should.equal('add')
+  e[1].sent.channel.should.equal('add_success')
+  e[1].sent.message.result.should.equal(12)
+
+  // Success expectation
+  e[2].received.channel.should.equal('add_success')
+  e[2].received.message.result.should.equal(12)
+  e[2].expectation.message.result.should.equal(12)
+  e[2].expectation.match.should.equal(true)
+
+  timeline = result.timelines[1]
+  timeline.world.label.should.equal('Test failure')
+  e = timeline.events
+
+  e[2].expectation.message.result.should.equal(13)
+  e[2].expectation.match.should.equal(false)
 
 })
-
+/*
 .then(function() {
   return runner.run({
     transforms: {
@@ -77,7 +127,7 @@ runner.run({
   timeline.events[0].sent.channel.should.equal('start')
   timeline.events[0].sent.message.should.equal(true)
 
-}).done(function() {
+})*/.done(function() {
   console.log("All is well.")
 })
 
